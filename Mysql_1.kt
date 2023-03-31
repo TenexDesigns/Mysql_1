8d844a79a7bf3e423a14517a0ae3e1e23ad57917
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-31 13:18:02 +0300

    Update Mysql_1.kt

--- Mysql_1.kt
+++ Mysql_1.kt
@@ -1,2 +1,17 @@
+The video tutorial is https://youtu.be/5OdVJbNCSso    BROCODE -MySql full course
 First we are going to create querries .
 This querrys are not case sensitive and can be in capital or small letter.
+
+TO CREATE A DATABASE IN MYSQL
+
+  -----> CREATE DATABASE myDb;  
+We have to end the query with a semicolon.
+
+
+TO USE A DATABASE
+
+-----> USE myDB
+
+TO DELETE OR DROP A DATABASE
+
+-----> DROP DATABASE myDb
