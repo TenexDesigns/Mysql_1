358e623f6aa5defb2bade1501872ab8d13a3af68
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-04-03 12:09:07 +0300

    Create Order_by_clause.kt

---
+++ Order_by_clause.kt
@@ -0,0 +1,46 @@
+This is used to arrange rows based on assending or decending oreder.
+
+
+This orders the rows in ascending oredr ,alphabeticaly ,from top to botom
+
+
+SELECT * FROM employyes ORDER BY last_name ;
+
+
+
+
+To change the oreder ,Maybe strat from DESC 
+ This arranges the rows in decending oreder
+
+SELECT * FROM employyes ORDER BY last_name  DESC;
+
+
+
+
+YOU CAN EVEN INCLIDE TWO PARST BY WHICH THE ORDER CAN HAPPEN
+
+
+SELECT * FROM transactions ORDER BY amount ,customer_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
